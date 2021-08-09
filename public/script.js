9a21fe7f768d270dfb3709ae4c9a62aecb731a56
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-08-09 00:43:44 -0300

    Add basic HTML

--- public/script.js
+++ public/script.js
@@ -1,15 +1,46 @@
 let myLibrary = [];
 
-function Book(name, author, pages, read) {
-  this.name = name;
+function Book(title, author) {
+  this.title = title;
   this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.displayBook = function () {
-    console.log(name, author, pages, read)
-  }
 }
 
-function addBookToLibrary() {
-  // do stuff here
+Book.prototype.addBookToLibrary = function () {
+  myLibrary.push(this);
+  setBooks();
 }
+
+function mapBooks(callback) {
+  myLibrary.map((book, index) => {
+    callback && callback(book, index);
+    return true;
+  });
+}
+
+function deleteWithIndex(index, callback) {
+  myLibrary.splice(index, 1);
+  callback && callback();
+  setBooks();
+}
+
+function getBooks () {
+  let data = localStorage.getItem('myLibrary');
+  data = JSON.parse(data);
+  const result = [];
+  data.map(book => {
+    const {
+      title,
+      author
+    } = book;
+    result.push(new Book(title, author));
+    return true;
+  });
+  return result;
+}
+
+function setBooks() {
+  const data = JSON.stringify(myLibrary);
+  localStorage.setItem('myLibrary', data);
+}
+
+myLibrary = getBooks();
